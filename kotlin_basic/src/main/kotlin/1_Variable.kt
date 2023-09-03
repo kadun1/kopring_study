d1351bb5005df1fdd51be70372783acd8540608f
kadun1 <kadunjj@gmail.com>
2023-09-03 20:32:44 +0900

    Kopring Study Start

---
+++ kotlin_basic/src/main/kotlin/1_Variable.kt
@@ -0,0 +1,27 @@
+var x = 5
+
+fun main() {
+
+    x += 1
+    print(x)
+
+    val a : Int = 1
+
+    val b = 1
+
+    val c : Int
+    c = 3
+
+    val d : Int
+    d = 123
+
+    //val(value)
+    //var(variable)
+
+    var e : String = "Hello"
+    e = "World"
+
+    var f = 123
+//    f = "hi"
+
+}
