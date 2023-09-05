1c1a6e0c4ee5c31dc84e368ee6d5475a37c9848b
kadun1 <kadunjj@gmail.com>
2023-09-05 21:33:13 +0900

    kotlin_basic 예외 처리

---
+++ kotlin_basic/src/main/kotlin/4_Exception.kt
@@ -0,0 +1,33 @@
+fun main() {
+
+    try {
+        throw Exception()
+    } catch (e: Exception) {
+        println("에러 발생")
+    } finally {
+        println("finally 실행!")
+    }
+
+    val a = try {
+        "1234".toInt()
+    } catch (e: Exception) {
+        println("예외 발생 !")
+    }
+
+    println(a)
+//    throw Exception("예외 발생!")
+
+//    failFast("예외 발생!")
+
+    var b : String? = null
+    val c : String = b ?: failFast("a is null")
+
+    println(c.length)
+
+    println("이 메세지를 출력될까?")
+
+}
+
+fun failFast(message: String): Nothing {
+    throw IllegalArgumentException(message)
+}
