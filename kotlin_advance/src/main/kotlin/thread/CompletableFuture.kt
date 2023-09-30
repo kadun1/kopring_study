c303a60dc25379eff9f48c08e991015bdc919f11
kadun1 <kadunjj@gmail.com>
2023-10-01 03:46:32 +0900

    kotlin_advance : non-blocking

---
+++ kotlin_advance/src/main/kotlin/thread/CompletableFuture.kt
@@ -0,0 +1,22 @@
+package thread
+
+import java.util.concurrent.CompletableFuture
+
+fun main() {
+    val completableFuture = CompletableFuture.supplyAsync {
+        Thread.sleep(2000)
+        sum(100, 200)
+    }
+
+    println("계산 시작")
+//    completableFuture.thenApplyAsync(::println) //논블로킹으로 동작
+
+    val result = completableFuture.get()
+    println(result)
+
+    while (!completableFuture.isDone) {
+        Thread.sleep(500)
+        println("계산 결과를 집계 중입니다.")
+    }
+    println("계산 종료")
+}
